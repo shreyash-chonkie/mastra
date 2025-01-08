1caf02299dd63a6863ad4edd9195961307861e64
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2025-01-08 17:01:31 +0100

    add correct agents

--- examples/mastra-prompt-testing/src/mastra/agents/index.ts
+++ examples/mastra-prompt-testing/src/mastra/agents/index.ts
@@ -1,11 +1,29 @@
 import { Agent } from '@mastra/core';
 
-export const AgentMastra = new Agent({
-  name: 'Agent Mastra',
+export const AgentMastraAnthropic = new Agent({
+  name: 'Agent Mastra Anthropic',
   instructions: 'You know everything about the world',
   model: {
     provider: 'ANTHROPIC',
     name: 'claude-3-5-sonnet-20241022',
+    toolChoice: 'auto',
+  },
+});
+export const AgentMastraOpenAI = new Agent({
+  name: 'Agent Mastra OpenAI',
+  instructions: 'You know everything about the world',
+  model: {
+    provider: 'OPEN_AI',
+    name: 'gpt-4o',
+    toolChoice: 'auto',
+  },
+});
+export const AgentMastraGroq = new Agent({
+  name: 'Agent Mastra Groq',
+  instructions: 'You know everything about the world',
+  model: {
+    provider: 'GROQ',
+    name: 'llama3-8b-8192',
     toolChoice: 'auto',
   },
 });
