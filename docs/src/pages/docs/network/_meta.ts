51a11cdd2b645c127efa8825249bd946d71d7732
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-03-06 22:36:36 -0800

    Pair back the docs

--- docs/src/pages/docs/network/_meta.ts
+++ docs/src/pages/docs/network/_meta.ts
@@ -1,8 +1,5 @@
 const meta = {
   "00-overview": "Overview",
-  "01-custom-routing": "Custom Routing",
-  "02-state-management": "State Management",
-  "03-streaming": "Streaming",
 };
 
 export default meta;
