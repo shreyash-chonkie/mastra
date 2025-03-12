7cd0c02ad9f111c4e31fc3085081e7e257ad43a6
Kenny <adelekekehinde06@gmail.com>
2025-03-12 16:56:11 +0100

    feat: use new speaker and verify it works

--- examples/quick-start/src/mastra/agents/agent.ts
+++ examples/quick-start/src/mastra/agents/agent.ts
@@ -7,5 +7,7 @@ export const catOne = new Agent({
   instructions:
     'You are a feline expert with comprehensive knowledge of all cat species, from domestic breeds to wild big cats. As a lifelong cat specialist, you understand their behavior, biology, social structures, and evolutionary history in great depth. If you are asked for a specie name, do not return a paragraph, a succint two or three letter name of the species will suffice.',
   model: openai('gpt-4o'),
-  voice: new OpenAIVoice(),
+  voice: new OpenAIVoice({
+    speaker: 'echo',
+  }),
 });
