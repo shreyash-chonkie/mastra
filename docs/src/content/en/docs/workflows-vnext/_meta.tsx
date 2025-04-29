92a2c15fdef82fc3e77de260c3392b1bc045e579
Tony Kovanen <tony.kovanen@gmail.com>
2025-04-29 12:42:40 +0300

    rename variable mapping -> input data mapping

--- docs/src/content/en/docs/workflows-vnext/_meta.tsx
+++ docs/src/content/en/docs/workflows-vnext/_meta.tsx
@@ -3,7 +3,7 @@ const meta = {
   overview: "Overview",
   "flow-control": "Flow Control",
   "suspend-and-resume": "Suspend & Resume",
-  "variable-mapping": "Variable Mapping",
+  "input-data-mapping": "Input Data Mapping",
   "using-with-agents-and-tools": "Using with Agents and Tools",
 };
 
