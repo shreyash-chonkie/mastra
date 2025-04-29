9965e79a9f3d263dea8cb172e3572a803ac4f80c
dayo <ifedayoadeniyi@gmail.com>
2025-04-29 11:08:58 +0100

    vNext workflows example (#3955)
    ## Description
    
    <!-- Provide a brief description of the changes in this PR -->
    
    ## Related Issue(s)
    
    <!-- Link to the issue(s) this PR addresses, using hashtag notation:
    #123 -->
    
    ## Type of Change
    
    - [ ] Bug fix (non-breaking change that fixes an issue)
    - [ ] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Documentation update
    - [ ] Code refactoring
    - [ ] Performance improvement
    - [ ] Test update
    
    ## Checklist
    
    - [ ] I have performed a self-review of my own code
    - [ ] I have made corresponding changes to the documentation (if
    applicable)
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] I have generated a changeset for this PR

---
+++ docs/src/content/en/examples/workflows_vNext/_meta.ts
@@ -0,0 +1,14 @@
+const meta = {
+  "creating-a-workflow": "Creating a Workflow",
+  "sequential-steps": "Sequential Steps",
+  "parallel-steps": "Parallel Steps",
+  "branching-paths": "Branching Paths",
+  "conditional-branching": "Conditional Branching",
+  "human-in-the-loop": "Human in the Loop",
+  "calling-agent": "Calling an Agent",
+  "suspend-and-resume": "Suspend and Resume",
+  "using-a-tool-as-a-step": "Using a Tool as a Step",
+  "variable-mapping": "Workflow Variables",
+};
+
+export default meta;
