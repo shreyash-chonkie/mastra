148e3469d2185221257f4ced68659ce4155af120
Tony Kovanen <tony.kovanen@gmail.com>
2025-04-29 10:46:43 +0300

    splite readme into docs

---
+++ docs/src/content/en/docs/workflows-vnext/_meta.tsx
@@ -0,0 +1,10 @@
+import { Tag } from "@/components/tag";
+const meta = {
+  overview: "Overview",
+  "flow-control": "Flow Control",
+  "suspend-and-resume": "Suspend & Resume",
+  "variable-mapping": "Variable Mapping",
+  "using-with-agents-and-tools": "Using with Agents and Tools",
+};
+
+export default meta;
